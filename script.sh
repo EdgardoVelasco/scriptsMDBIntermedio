193d4650efe785405fbc1784c4138e365e390a7e
Edgardo Velasco <edgardo.nvf@gmail.com>
2023-08-21 17:28:42 -0600

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir nodo1 nodo2 nodo3
+cd nodo1
+mkdir db logs
+cd logs
+touch mongod.log
+cd ..
+cd ..
+cd nodo2
+mkdir db logs
+cd logs
+touch  mongod.log
+cd ..
+cd ..
+cd nodo3
+mkdir db logs
+cd logs
+touch mongod.log
+cd ..
+cd ..
+echo "estructua creada"
